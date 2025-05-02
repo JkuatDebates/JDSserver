cf67ce3d25aa72db25115ac83a85cb0fd25091f0
JkuatDebates <jkuatdebates@gmail.com>
2025-05-02 05:52:47 +0300

    cors update

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ connectDB();//.then(()=>console.log("DB connected"));
 
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://jdsweb.netlify.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }));
 app.use(express.urlencoded({extended: false}));
 
 //routes
